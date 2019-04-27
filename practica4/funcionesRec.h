e0e0e6212d5fc624c5828414286f79ef19763014
ManuelB <benitez.emilio.morales@gmail.com>
2019-04-26 23:49:44 -0500

    cambios mamalones :v

--- practica4/funcionesRec.h
+++ practica4/funcionesRec.h
@@ -1,13 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void menu()
-{
-  printf("BIENVENIDO A LA PRACTICA 4, SOLUCIONES RECURSIVAS\n");
-  printf("Por favor elije el problema que deseas ver:\n");
-  printf("1. Serie de Fibonacci.");
-  printf("2. Serie de Tribonacci.");
-  printf("3. Torres de Hanoi.");
-  printf("4. Ordenamiento por mezcla.");
-  return;
-}
+/*
+
+*/
+void menu();
--- practica4/main.c
+++ practica4/main.c
@@ -3,6 +3,31 @@
 
 int main()
 {
+  int opc=0;
+
+  menu();
+  scanf("%d", &opc);
+
+  do
+  {
+    switch (opc) {
+      case 1:
+
+      break;
+
+      case 2:
+
+      break;
+
+      case 3:
+
+      break;
+
+      case 4:
+
+      break;
+    }//switch
+  }while(opc<5);
 
 
   return 0;
