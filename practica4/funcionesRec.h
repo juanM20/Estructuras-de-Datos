1b867165185c1d07465cc0723c259e1ba932b819
ManuelB <benitez.emilio.morales@gmail.com>
2019-04-26 23:17:52 -0500

    menu

--- practica4/funcionesRec.h
+++ practica4/funcionesRec.h
@@ -3,6 +3,11 @@
 
 void menu()
 {
-  printf("BIENVENIDO A LA PRACTICA 4, SOLUCIONES RECURSIVAS");
+  printf("BIENVENIDO A LA PRACTICA 4, SOLUCIONES RECURSIVAS\n");
+  printf("Por favor elije el problema que deseas ver:\n");
+  printf("1. Serie de Fibonacci.");
+  printf("2. Serie de Tribonacci.");
+  printf("3. Torres de Hanoi.");
+  printf("4. Ordenamiento por mezcla.");
   return;
 }
