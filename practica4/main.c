014fcd78f7951ba6365945c113318fe9a64cd9dd
ManuelB <benitez.emilio.morales@gmail.com>
2019-04-27 23:51:20 -0500

    sdfsd

--- practica4/main.c
+++ practica4/main.c
@@ -14,25 +14,21 @@ int main()
       case 1:
         printf("San fibonacci :v");
         system("cls");
-        Menu();
       break;
 
       case 2:
         printf("San tribonacci :v");
         system("cls");
-        Menu();
       break;
 
       case 3:
         printf("San Hanoi :v");
         system("cls");
-        Menu();
       break;
 
       case 4:
         printf("Santa mezcla ordenada alv :v");
         system("cls");
-        Menu();
       break;
     }//switch
   }
