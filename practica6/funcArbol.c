e378291e80d22b16e3949a03d2ac7cf0ca15f7cb
ManuelB <benitez.emilio.morales@gmail.com>
2019-05-30 17:24:24 -0500

    Nuevo

--- practica6/funcArbol.c
+++ practica6/funcArbol.c
@@ -62,3 +62,85 @@ void Cargar_Archivo()
 
   }*/
 }
+
+void imprimirArbol(struct nodo *ptrRaiz,int contador)
+{
+	int i,j;
+	i=contador;
+	if(ptrRaiz!=NULL)
+	{
+		imprimirArbol(ptrRaiz->ptrDer,i+1);
+		printf("\n");
+		for(j=1;j<i;j++)
+			printf("-");
+		printf("%d",ptrRaiz->dato);
+		imprimirArbol(ptrRaiz->ptrIzq,i+1);
+	}
+}
+
+
+
+struct nodo* balancear(struct nodo *ptr){
+  calculaAltura(ptr);
+
+     if ( altura(ptr -> ptrIzq) - altura(ptr -> ptrDer) == 2 )
+     {
+       printf("\nSe rota a la derecha" );
+         if ( altura(ptr -> ptrIzq -> ptrDer) > altura(ptr -> ptrIzq -> ptrIzq) )
+             ptr -> ptrIzq = rota_izquierda(ptr -> ptrIzq);
+         return rota_derecha(ptr);
+     }
+     else if ( altura(ptr -> ptrDer) - altura(ptr -> ptrIzq) == 2 )
+     {
+       printf("\nSe rota a la izquierda" );
+         if ( altura(ptr -> ptrDer -> ptrIzq) > altura(ptr -> ptrDer -> ptrDer) )
+             ptr -> ptrDer = rota_derecha(ptr -> ptrDer);
+         return rota_izquierda(ptr);
+     }
+     printf("\n %d",ptr->bal );
+     return ptr;
+}
+
+
+void calculaAltura(struct nodo*p)
+{
+	p->bal=1+maximo(altura(p->ptrIzq),altura(p->ptrDer));
+}
+
+struct nodo* rota_izquierda(struct nodo* ptr){
+    struct nodo * temp;
+    temp = ptr -> ptrDer;
+    ptr -> ptrDer = temp -> ptrIzq;
+    temp -> ptrIzq = ptr;
+    calculaAltura(ptr);
+    calculaAltura(temp);
+    return temp;
+}
+
+struct nodo* rota_derecha(struct nodo* ptr){
+    struct nodo *temp;
+    temp = ptr -> ptrIzq;
+    ptr -> ptrIzq = temp -> ptrDer;
+    temp -> ptrDer = ptr;
+    calculaAltura(ptr);
+    calculaAltura(temp);
+    return temp;
+}
+
+
+int maxProfundidad(struct nodo*ptrRaiz)
+{
+	int profIzq, profDer;
+	if(ptrRaiz==NULL)
+	return 0;
+	else
+	{
+		profIzq=maxProfundidad(ptrRaiz->ptrIzq);
+   profDer=maxProfundidad(ptrRaiz->ptrDer);
+   if(profIzq>profDer)
+    return (profIzq+1);
+   else
+     return (profDer+1);
+  }
+
+}
--- practica6/funcArbol.h
+++ practica6/funcArbol.h
@@ -3,4 +3,20 @@
 #include <string.h>
 #include "TADArbolBin.h"
 
+/*
+struct nodo{
+	int dato;
+	int bal;
+	struct nodo *ptrIzq;
+	struct nodo *ptrDer;
+};
+*/
+int altura(struct nodo *ptr);
+elemento balancear (struct nodo *ptrRaiz);
+//void insertar(struct nodo **ptrRaiz, int miDato1);
+void imprimirArbol(struct nodo *ptrRaiz, int contador);
+void calculaAltura(struct nodo*p);
+int maxProfundidad(struct nodo *ptrRaiz);
+elemento rota_derecha(struct nodo*ptr);
+elemento rota_izquierda(struct nodo*ptr);
 int Menu();
