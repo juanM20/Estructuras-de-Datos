df704dab9807712b7e61e0d87199f7bedbc17a75
ManuelB <benitez.emilio.morales@gmail.com>
2019-05-30 19:52:47 -0500

    Lista la carga del archivo

--- practica6/funcArbol.c
+++ practica6/funcArbol.c
@@ -16,7 +16,9 @@ void Menu()
 void Cargar_Archivo(char* ruta, FILE* archivo)
 {
   //printf("%s", ruta);
-
+  int i=0, j=0; //variables que nos ayudarán a indicar los indices de las cadenas donde se extraerán las palabras y definiciones.
+  char cad_aux[TAM_CADENA];//cadena auxiliar que nos permitira extraer las palabras junto con su definicion.
+  char palabra[TAM_PALABRA], definicion[TAM_DEF];// p es la palabra que se guardará y def es la definicion.
   archivo = fopen(ruta,"r");
   if(archivo == NULL)
     printf("\nERROR: NO SE PUEDE ABRIR EL ARCHIVO");
@@ -47,14 +49,14 @@ void Cargar_Archivo(char* ruta, FILE* archivo)
       }
 
       definicion[i] = '\0';
-
+/*
       indice = sumaCaracteres(&palabra[0])%TAM_TABLA;
       if(Tam_Lista(&colisiones[indice]) > 1) num_colisiones++;
       Agregar_Definicion(&colisiones[indice],&palabra[0],&definicion[0]);
       printf("\nIndice: %d \t%s : %s",indice, palabra, definicion);
-
+*/
     }
-
+    printf("\nARCHIVO CARGADO CON EXITO!!!");
   }
 }
 /*
